c4af1b690667fafdcab9798e1a55be9094fd0560
Vivek Maskara <maskaravivek@gmail.com>
2022-02-12 14:13:43 -0700

    With lambda function deployed

--- bin/aws-cdk-lambda-circle-ci.ts
+++ bin/aws-cdk-lambda-circle-ci.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
+import * as cdk from "@aws-cdk/core";
 import { AwsCdkLambdaCircleCiStack } from '../lib/aws-cdk-lambda-circle-ci-stack';
 
 const app = new cdk.App();
--- lib/aws-cdk-lambda-circle-ci-stack.ts
+++ lib/aws-cdk-lambda-circle-ci-stack.ts
@@ -1,16 +1,39 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "@aws-cdk/core";
+import * as s3 from "@aws-cdk/aws-s3";
+import * as dynamodb from "@aws-cdk/aws-dynamodb";
+import * as lambda from "@aws-cdk/aws-lambda";
+import { Duration } from "@aws-cdk/core";
 
-export class AwsCdkLambdaCircleCiStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class AwsCdkLambdaCircleCiStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const circleCiGwpBucket = new s3.Bucket(this, "circle-ci-gwp-bucket", {
+      bucketName: "circle-ci-gwp",
+      publicReadAccess: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkLambdaCircleCiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const circleCiGwpTable = new dynamodb.Table(this, "CircleCIGwpTable", {
+      tableName: "CircleCIGwpTable",
+      partitionKey: { name: "jobId", type: dynamodb.AttributeType.STRING },
+    });
+
+    const circleCiGwpLambda = new lambda.Function(
+      this,
+      "CircleCiGwpLambda",
+      {
+        runtime: lambda.Runtime.NODEJS_14_X,
+        handler: "index.handler",
+        timeout: Duration.seconds(30),
+        code: lambda.Code.fromAsset("lambda/"),
+        environment: {
+          TABLE_NAME: circleCiGwpTable.tableName,
+          BUCKET_NAME: circleCiGwpBucket.bucketName
+        },
+      }
+    );
+
+    circleCiGwpTable.grantReadWriteData(circleCiGwpLambda);
+    circleCiGwpBucket.grantPut(circleCiGwpLambda)
   }
 }
