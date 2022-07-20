1f44edc2c8f0055303d0b6d5589a9126f3ed480a
Vivek Maskara <maskaravivek@gmail.com>
2022-07-20 16:03:33 -0400

    Update lambda code

--- lib/aws-cdk-lambda-circle-ci-stack.ts
+++ lib/aws-cdk-lambda-circle-ci-stack.ts
@@ -1,16 +1,43 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import {
+  Stack,
+  StackProps,
+  aws_s3 as s3,
+  aws_dynamodb as dynamodb,
+  aws_lambda as lambda,
+  Duration
+} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsCdkLambdaCircleCiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // we will add all the constructs here
+    const circleCiGwpBucket = new s3.Bucket(this, "circle-ci-gwp-bucket", {
+      bucketName: "circle-ci-gwp",
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkLambdaCircleCiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const circleCiGwpTable = new dynamodb.Table(this, "CircleCIGwpTable", {
+      tableName: "CircleCIGwpTable",
+      partitionKey: { name: "jobId", type: dynamodb.AttributeType.STRING },
+    });
+
+    const circleCiGwpLambda = new lambda.Function(
+      this,
+      "CircleCiGwpLambda",
+      {
+        runtime: lambda.Runtime.NODEJS_14_X,
+        handler: "index.handler",
+        timeout: Duration.seconds(30),
+        code: lambda.Code.fromAsset("lambda/"),
+        environment: {
+          TABLE_NAME: circleCiGwpTable.tableName,
+          BUCKET_NAME: circleCiGwpBucket.bucketName
+        },
+      }
+    );
+
+    circleCiGwpBucket.grantPut(circleCiGwpLambda);
+    circleCiGwpTable.grantReadWriteData(circleCiGwpLambda);
   }
 }
